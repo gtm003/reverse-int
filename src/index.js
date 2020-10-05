f77bd48181b8c889f5ad37fcd1ec748d61ecdf48
Grigorovich Tatyana <gtm003@gmail.com>
2020-10-05 23:19:50 +0300

    feat: add function reverse

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+    let str = String(Math.abs(n)),
+    result = 0;
+for (i=str.length-1; i >=0 ;i--) {
+result = result + str[i];
+}
+return +result;
 }
